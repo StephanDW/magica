83129a775b4aed3e6eba848877a2e416d74efbbb
Steviee3 <123730716+Steviee3@users.noreply.github.com>
2023-11-19 19:36:49 +0200

    changes to gameplay

--- Scripts/Levels/Level1/level_1.gd
+++ Scripts/Levels/Level1/level_1.gd
@@ -3,6 +3,10 @@ extends Node2D
 var GODOT
 var PLAYER
 
+var elem1
+var elem2
+var elem3
+
 func _ready():
 	GODOT = bosses.new()
 	PLAYER = player.new()
@@ -12,10 +16,32 @@ func _ready():
 	print(GODOT.hp)
 
 func _on_button_pressed():
-	GODOT.BossDamage("",10)
+	var damage = 0
+	
+	if (elem1):
+		damage += 10
+		
+	if (elem2):
+		damage += 10
+		
+	if (elem3):
+		damage += 10
+	
+	GODOT.BossDamage("",damage)
 	if (GODOT.hp <= 0):
 		get_tree().change_scene_to_file("res://scenes/level_end.tscn")
 	
 	PLAYER.playerDamage(5)
 	if (PLAYER.hp <= 0):
 		get_tree().change_scene_to_file("res://scenes/level_end.tscn")
+
+func _on_elem_1_toggled(button_pressed):
+	elem1 = button_pressed
+
+
+func _on_elem_2_toggled(button_pressed):
+	elem2 = button_pressed
+
+
+func _on_elem_3_toggled(button_pressed):
+	elem3 = button_pressed
